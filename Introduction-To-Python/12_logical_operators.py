51d99815f35dd732eea0edeacf979f02074c1236
Suwadith <suwadith.srithar@gmail.com>
2019-03-04 12:35:44 +0530

    spell fix

--- Introduction-To-Python/12_logical_operators.py
+++ Introduction-To-Python/12_logical_operators.py
@@ -1,6 +1,6 @@
 has_high_income = False
 has_good_credit = True
-has_criminal_reord = True
+has_criminal_record = True
 
 if has_high_income and has_good_credit:
     print("Eligible for loan - 1")
@@ -8,7 +8,7 @@ if has_high_income and has_good_credit:
 if has_high_income or has_good_credit:
     print("Eligible for loan - 2")
 
-if has_good_credit and not has_criminal_reord:
+if has_good_credit and not has_criminal_record:
     print("Eligible for loan - 3")
 
 # AND: both
