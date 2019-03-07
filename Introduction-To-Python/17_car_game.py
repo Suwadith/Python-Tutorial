017d7e8809904209b42211f38f157495303eb173
Suwadith <suwadith.srithar@gmail.com>
2019-03-07 23:23:48 +0530

    update

--- Introduction-To-Python/17_car_game.py
+++ Introduction-To-Python/17_car_game.py
@@ -1,16 +1,26 @@
 command = ""
+car_is_started = False
+
 while command != "quit":
     command = input("> ").lower()
     if command == "help":
         print('''
-        start - to start the car
-        stop -to stop the car
-        quit - to exit
+start - to start the car
+stop -to stop the car
+quit - to exit
         ''')
     elif command == "start":
-        print("car started...")
+        if car_is_started:
+            print("car is already started...")
+        else:
+            car_is_started = True
+            print("car started...")
     elif command == "stop":
-        print("car stopped...")
+        if not car_is_started:
+            print("car has already been stopped...")
+        else:
+            car_is_started = False 
+            print("car stopped...")
     elif command == "quit":
         break
     else:
---
+++ Introduction-To-Python/18_for_loops.py
@@ -0,0 +1,24 @@
+for character in "Python":
+    print(character)
+
+for name in ['Mosh', 'John', 'Chris']:
+    print(name)
+
+for number in [1, 2, 3]:
+    print(number)
+
+for number in range(10):
+    print(number)
+
+for number in range(5, 10):
+    print(number)
+
+for number in range(5, 10, 2):
+    print(number)
+
+prices = [10, 20, 30]
+total = 0;
+
+for price in prices:
+    total += price
+print(f"Total: {total}")
