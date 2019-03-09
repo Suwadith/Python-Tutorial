a8c8f4c14b25e7c34bf17c4becee7e0f59cc7674
Suwadith <suwadith.srithar@gmail.com>
2019-03-10 01:35:05 +0530

    update

---
+++ Introduction-To-Python/26_functions.py
@@ -0,0 +1,9 @@
+def greet_user(first_name, last_name):
+    print(f'Hi there {first_name} {last_name}!')
+    print('Welcome aboard')
+
+
+print("Start")
+greet_user("Henry", "Josh")
+greet_user("Heather" ,"Mary")
+print("Finish")
